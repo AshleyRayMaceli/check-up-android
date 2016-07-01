09fa90e8af671ebbcf5d1e04f714ccf87b228b04
ashleyraymaceli <ashley.maceli@gmail.com>
2016-07-01 10:15:34 -0700

    add placeholder content to AppointmentsListView, add onClick() to navigate to AppointmentFormActivity

--- app/src/main/java/com/epicodus/checkup/AppointmentsActivity.java
+++ app/src/main/java/com/epicodus/checkup/AppointmentsActivity.java
@@ -1,13 +1,35 @@
 package com.epicodus.checkup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class AppointmentsActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class AppointmentsActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.appointmentsListView) ListView mAppointmentsListView;
+    @Bind(R.id.newAppointmentButton) Button mNewAppointmentButton;
+
+    private String[] appointments = new String[] {"Doctor A - Jan 1st 2017", "Doctor B - Feb 5th 2017", "Doctor C - Feb 19th 2017", "Doctor D - March 1st 2017"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointments);
+        ButterKnife.bind(this);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, appointments);
+        mAppointmentsListView.setAdapter(adapter);
+        mNewAppointmentButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(AppointmentsActivity.this, AppointmentFormActivity.class);
+        startActivity(intent);
     }
 }
