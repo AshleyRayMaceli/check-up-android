90048cd581467650546f6184b7d0e7522456387c
ashleyraymaceli <ashley.maceli@gmail.com>
2016-07-01 09:41:35 -0700

    add AppointmentsActivity and navigation from MainActivity

--- app/src/main/java/com/epicodus/checkup/MainActivity.java
+++ app/src/main/java/com/epicodus/checkup/MainActivity.java
@@ -1,13 +1,30 @@
 package com.epicodus.checkup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.appointmentsButton) Button mAppointmentsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mAppointmentsButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mAppointmentsButton) {
+            Intent intent = new Intent(MainActivity.this, AppointmentsActivity.class);
+            startActivity(intent);
+        }
     }
 }
