1066ff33a7e879fffb5b8e5bf87e3fc1b5b5336b
ashleyraymaceli <ashley.maceli@gmail.com>
2016-07-01 14:43:48 -0700

    add functionality to HealthActivity for user to enter ailmet and have it display on the same activity

--- app/src/main/java/com/epicodus/checkup/HealthActivity.java
+++ app/src/main/java/com/epicodus/checkup/HealthActivity.java
@@ -1,24 +1,42 @@
 package com.epicodus.checkup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class HealthActivity extends AppCompatActivity {
+public class HealthActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.newAilmentInput) EditText mNewAilmentInput;
+    @Bind(R.id.addAilmentButton) Button mAddAilmentButton;
     @Bind(R.id.ailmentsListView) ListView mAilmentsListView;
-
-    private String[] ailments = new String[] {"Suspicious mole on arm", "Cannot fall asleep easily", "Cold that won't go away"};
+    private ArrayList<String> allUserAilments = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health);
         ButterKnife.bind(this);
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, ailments);
+        mAddAilmentButton.setOnClickListener(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, allUserAilments);
         mAilmentsListView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mAddAilmentButton) {
+            String ailment = mNewAilmentInput.getText().toString();
+            allUserAilments.add(ailment);
+            mNewAilmentInput.setText("");
+        }
     }
 }
