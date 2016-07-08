f5bb39bacd4f59b788161afd7785cc1bc136523e
ashleyraymaceli <ashley.maceli@gmail.com>
2016-07-08 09:32:25 -0700

    update FindDoctorActivity to accept a specialty from the user and pass that extra with the intent to DoctorActivity

--- app/src/main/java/com/epicodus/checkup/ui/FindDoctorActivity.java
+++ app/src/main/java/com/epicodus/checkup/ui/FindDoctorActivity.java
@@ -1,15 +1,37 @@
 package com.epicodus.checkup.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.epicodus.checkup.R;
 
-public class FindDoctorActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class FindDoctorActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Bind(R.id.specialtySearchButton) Button mSpecialtySearchButton;
+    @Bind(R.id.specialtyEditText) EditText mSpecialtyEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_doctor);
+        ButterKnife.bind(this);
+        mSpecialtySearchButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mSpecialtySearchButton) {
+            String specialty = mSpecialtyEditText.getText().toString();
+            Intent intent = new Intent(FindDoctorActivity.this, DoctorActivity.class);
+            intent.putExtra("specialty", specialty);
+            startActivity(intent);
+        }
     }
 }
