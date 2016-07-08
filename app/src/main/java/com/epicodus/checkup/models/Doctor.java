77356015bdcc08bfceb379e7a0a8c54323489dec
ashleyraymaceli <ashley.maceli@gmail.com>
2016-07-08 10:59:49 -0700

    add Doctor model to construct Doctor objects from API search

---
+++ app/src/main/java/com/epicodus/checkup/models/Doctor.java
@@ -0,0 +1,58 @@
+package com.epicodus.checkup.models;
+
+import java.util.ArrayList;
+
+public class Doctor {
+    private String mName;
+    private String mSpecialty;
+    private String mPhone;
+    private double mLatitude;
+    private double mLongitude;
+    private ArrayList<String> mAddress = new ArrayList<>();
+    private ArrayList<String> mLanguages = new ArrayList<>();
+    private String mBio;
+
+    public Doctor(String name, String specialty, String phone, double latitude, double longitude, ArrayList<String> address, ArrayList<String> languages, String bio) {
+        this.mName = name;
+        this.mSpecialty = specialty;
+        this.mPhone = phone;
+        this.mLatitude = latitude;
+        this.mLongitude = longitude;
+        this.mAddress = address;
+        this.mLanguages = languages;
+        this.mBio = bio;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getSpecialty() {
+        return mSpecialty;
+    }
+
+    public String getPhone() {
+        return mPhone;
+    }
+
+    public double getLatitude() {
+        return mLatitude;
+    }
+
+    public double getLongitude() {
+        return mLongitude;
+    }
+
+    public ArrayList<String> getAddress() {
+        return mAddress;
+    }
+
+    public ArrayList<String> getLanguages() {
+        return mLanguages;
+    }
+
+    public String getBio() {
+        return mBio;
+    }
+
+}
